e7b920aa6175262bd6c494ec64d35dfd2c7937d1
amitiz <sthaamit63@gmail.com>
2025-03-21 18:38:25 +1100

    final

--- script.js
+++ script.js
@@ -33,8 +33,8 @@ const createBotResponse = async (incomingMessageDiv) => {
     const messageElement = incomingMessageDiv.querySelector(".message-text");
 
     try {
-        const response = await fetch('http://localhost:5001/chat', {
-            method: 'POST',
+        const response = await fetch("http://localhost:5002/chat", {
+            method: "POST",
             headers: { "Content-Type": "application/json" },
             body: JSON.stringify({ message: userData.message, file: userData.file.data ? userData.file: null }),
         })
